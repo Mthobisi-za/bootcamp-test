de87e4d3bd3fd00e891e6354a3960cdf4d50787f
Mthobisi-za <70770544+Mthobisi-za@users.noreply.github.com>
2021-04-21 02:10:15 +1400

    Update regcheck.test.js

--- test/regcheck.test.js
+++ test/regcheck.test.js
@@ -1,9 +1,9 @@
 
 describe('regCheck', () => {
-    it("Check for Gp registrations and return true",()=>{
+    it("Check for Gp registrations and return true if passed arguments are from GP",()=>{
         assert.equal(regCheck("DC 55 YU GP", "GP"), true, "Function must check if registration number is for GP");
     });
-    it("Check for GP return false",()=>{
+    it("Check for GP return false if passed argument is not from Gp",()=>{
         assert.equal(regCheck("DC 55 YU GP", "G"), false, "Function must check if registration number is for GP.");
     });
 });
