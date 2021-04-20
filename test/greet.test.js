4f3a424d3d771b3ef74fe381c8811a54f1d83679
Mthobisi-za <mthobisimtho333@gmail.com>
2021-04-21 01:45:08 +1400

    greet

--- test/greet.test.js
+++ test/greet.test.js
@@ -2,13 +2,13 @@
 
 
 describe("Greet", ()=>{
-    it("should be able to greet Jane", ()=>{
-        assert.equal(greet("Jane"), "Hello, Jane", "The function should Return Hello Jane");  
+    it("The function should be able to greet Jane", ()=>{
+        assert.equal(greet("Jane"), "Hello, Jane", " return Hello Jane");  
     });
-    it("should be able to greet Mtho", ()=>{
-        assert.equal(greet("Mtho"), "Hello, Mtho", "The function should Return Hello Mtho");
+    it("The function should be able to greet Mtho", ()=>{
+        assert.equal(greet("Mtho"), "Hello, Mtho", "Return Hello Mtho");
     });
-    it("should be able to greet John", ()=>{
-        assert.equal(greet("John"), "Hello, John", "The function should Return Hello john");
+    it("The function should be able to greet John", ()=>{
+        assert.equal(greet("John"), "Hello, John", "Return Hello john");
     });
 })
